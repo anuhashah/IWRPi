42503988b5399965bb4b896381ed06cf76c97e97
Anuja Shah <apshah@usc.edu>
2022-02-06 19:50:57 -0800

    Initial Commit

--- IWRPi_M3A.py
+++ IWRPi_M3A.py
@@ -8,7 +8,7 @@
 # response to return the contents of the webpage.
 
 import http.client
-conn = http.client.HTTPConnection("www.uci.edu") # performs DNS Lookup and establishes connection
+conn = http.client.HTTPConnection("uci.edu") # performs DNS Lookup and establishes connection
 conn.request("GET", "/") # sends request w/ key details and composes message and sends
 response = conn.getresponse() # gets response 
 print(response.status, response.reason) # Status code and reason phrase returned by server 
