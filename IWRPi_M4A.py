4dfa4009f3abdd5e1df9e31e668e324889a365d9
Anuja Shah <apshah@usc.edu>
2022-02-15 23:51:21 -0800

    slower

--- IWRPi_M4A.py
+++ IWRPi_M4A.py
@@ -8,22 +8,23 @@
 # Link to demo video: 
 
 # R > (3.3-1.8/0.02) = 75 --> 3.3 is high voltage of pin set as output, 1.8 is voltage drop across LED, and 0.02 is the maximum current of the led
+# using 270 Ohm resistor
 
 import RPi.GPIO as GPIO # Import GPIO library so you can manipulate general purpose pins
 import time # Import time library to use sleep function
 
 GPIO.setmode(GPIO.BOARD) # Could be .BOARD (pin numbering) or .BCM (Broadcom SOC channel names)
-GPIO.setup(12, GPIO.OUT) # Set pin 12 on BOARD as output 
-pwm = GPIO.PWM(12, 50) # Initialize PWM on pwmPin (12) at 50 kHz frequency 
+GPIO.setup(18, GPIO.OUT) # Set pin 18 on BOARD as output 
+pwm = GPIO.PWM(18, 50) # Initialize PWM on pwmPin (18) at 50 kHz frequency 
 pwm.start(0) # Start PWM with 0% duty cycle (low)
 
 for i in range(0, 101, 1): # Loop from 0 to 99 (low to high) --> setting 100 throws ValueError: dutycyle must have value from 0.0 to 100.0
 	pwm.ChangeDutyCycle(i)
-	time.sleep(0.1) # Wait for 0.01 seconds at current LED brightness, so that it takes approx. 1 second to go from min to max brightness
+	time.sleep(0.01) # Wait for 0.01 seconds at current LED brightness, so that it takes approx. 1 second to go from min to max brightness
 
 for i in range(101, 0, -1): # Loop from 99 to 0 (decrements by 1)
 	pwm.ChangeDutyCycle(i)
-	time.sleep(0.1) # Approx. 1 second to go from max to min brightness
+	time.sleep(0.01) # Approx. 1 second to go from max to min brightness
 
 pwm.stop() # Stop PWM
 GPIO.cleanup() # Resets GPIO ports used back to input mode
