9899335c22ef7322712b4b1c8e0730372aa277d7
Anuja Shah <apshah@usc.edu>
2022-02-15 22:25:01 -0800

    Final Project

--- IWRPi_M4A.py
+++ IWRPi_M4A.py
@@ -15,7 +15,7 @@ GPIO.setup(12, GPIO.OUT) # Set pin 12 on BOARD as output
 pwm = GPIO.PWM(12, 50) # Initialize PWM on pwmPin (12) at 50 kHz frequency 
 pwm.start(0) # Start PWM with 0% duty cycle (low)
 
-for i in range(101): # Loop from 0 to 100 (low to high)
+for i in range(0, 101, 1): # Loop from 0 to 100 (low to high)
 	pwm.ChangeDutyCycle(i)
 	time.sleep(0.01) # Wait for 0.01 seconds at current LED brightness, so that it takes approx. 1 second to go from min to max brightness
 
